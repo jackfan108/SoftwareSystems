a4987076d9f05d29099e410fccc4b7ba92949c41
Jack Fan <jackfan108@gmail.com>
2015-09-11 23:51:24 -0400

    find_track regex implemented

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,8 +8,13 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
+
+regex_t regexp; // Regular Expression variable
+regmatch_t matches[NUM_TRACKS];    // Array of matches
+int status;                 // Check if regular expression compiles correctly
 
 char tracks[][80] = {
     "So What",
@@ -23,13 +28,13 @@ char tracks[][80] = {
 // Finds all tracks that contain the given string.
 //
 // Prints track number and title.
-void find_track(char search_for[])
+void find_track(char* search_for)
 {
     int i;
     for (i=0; i<NUM_TRACKS; i++) {
-	if (strstr(tracks[i], search_for)) {
-	    printf("Track %i: '%s'\n", i, tracks[i]);
-	}
+        if (strstr(tracks[i], search_for)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
     }
 }
 
@@ -37,8 +42,18 @@ void find_track(char search_for[])
 //
 // Prints track number and title.
 void find_track_regex(char pattern[])
-{
-    // TODO: fill this in
+{   
+    status = regcomp(&regexp, pattern, REG_EXTENDED); // compile the pattern with the regular expression and see if it's valid
+    printf("status: %d\n", status);
+
+    for (int i = 0; i < NUM_TRACKS; i++) {
+        // match every single track with the regex
+        status = regexec(&regexp, tracks[i], NUM_TRACKS, matches, 0);
+        if (status == 0) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+    
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -46,21 +61,37 @@ void rstrip(char s[])
 {
     char *ptr = strchr(s, '\n');
     if (ptr) {
-	*ptr = '\0';
+    *ptr = '\0';
     }
 }
 
 int main (int argc, char *argv[])
 {
     char search_for[80];
+    char option[2];
 
     /* take input from the user and search */
     printf("Search for: ");
     fgets(search_for, 80, stdin);
+    printf("\n");
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    printf("type 'a' for string search and 'b' for regex search: ");
 
+    
+    fgets(option, 2, stdin);
+    printf("\n");
+
+    if (*option == 'a') {
+        find_track(search_for);
+    }
+    else if (*option == 'b') {
+        find_track_regex(search_for);
+    }
+
+
+
+    
+    regfree(&regexp); // free up memory held within the regex memory
     return 0;
 }
